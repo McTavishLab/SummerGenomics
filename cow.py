b778e5f12403914df8b7f87480c7b1ceebc36081
Abby <52292344+acjones264@users.noreply.github.com>
2019-07-02 00:29:04 -0700

    Update cow.py

--- cow.py
+++ cow.py
@@ -12,20 +12,25 @@ g_count = file_contents.count('G')
 print("The number of guanine bases is " + str(g_count))
 g_content = ((g_count) / (bos_length)) * 100
 # divides the amount of Gs by the length of the entire fragment
-print{"The proportion of guanine in the sample is " + str(g_content) + "%"}
+g_round = round(g_content, 2)
+# rounds to 2 decimal places
+print ("The proportion of guanine in the sample is " + str(g_round) + "%")
 # add str to numerical values when printing to avoid syntax error
 
 a_count = file_contents.count('A')
 print("The number of adenine bases is " + str(a_count))
 a_content = ((a_count / bos_length)) * 100
-print{"The proportion of adenine in the sample is " + str(a_content) + "%"}
+a_round = round(a_content, 2)
+print ("The proportion of adenine in the sample is " + str(a_round) + "%")
 
 t_count = file_contents.count('T')
 print("The number of thymine bases is " + str(t_count))
 t_content = ((t_count / bos_length)) * 100
-print{"The proportion of thymine in the sample is " + str(t_content) +"%"}
+t_round = round(t_content, 2)
+print("The proportion of thymine in the sample is " + str(t_round) +"%")
 
 c_count = file_contents.count('C')
 print("The number of cytosine bases is " + str(c_count))
 c_content = ((c_count) / (bos_length)) * 100
-print{"The proportion of cytosine in the sample is " + str(c_content)+"%"}
+c_round = round(c_content, 2)
+print("The proportion of cytosine in the sample is " + str(c_round)+"%")
