2295729b592b5bb12995f6933791a4e02656180d
Emily Jane McTavish <ejmctavish@gmail.com>
2019-07-02 16:33:13 -0700

    comments for future work

--- ncbi.sh
+++ ncbi.sh
@@ -5,10 +5,12 @@
 for i in $( cat TableS2.csv ); 
     do
         SRSNUM=$(echo $i | cut -d ',' -f 7 | grep SRS)
-        #if CHECK IF vaiable has content?,THEN do the next steps
+        #if CHECK IF variable SRSNUM has content?, THEN do the next steps
         echo srsnum is $SRSNUM
         SRR=$(esearch -db sra -query $SRSNUM | efetch --format runinfo | cut -d ',' -f 1 | grep SRR)
         echo srr is $SRR >> srrnumbers.txt
+        #Prefetch those files
+        # And then fastq-dump them? Myabe as part of a differnet script?
     done
 
 
