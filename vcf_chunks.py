c6a5bbc33e6a5f3f6bc6e9ae753504961528bc76
Joseline Velasquez <joselinev534@gmail.com>
2019-11-14 11:15:25 -0800

    updated again

--- vcf_chunks.py
+++ vcf_chunks.py
@@ -1,13 +1,13 @@
 import sys
-import gzip 
+import gzip
 #when inputing into terminal put python2 p.py vcf.gz gene.vcf.gz chromesome.name start# end#
 #input files & outfile
 infile= sys.argv[1]#name of vcf
 outfile= sys.argv[2]#outfilename
-look= sys.argv[3] #This input should be the chromosome name 
-start= sys.argv[4] #This input should be the position at which to start 
-end= sys.argv[5]#This should be the position at which to end 
-#opening file 
+look= sys.argv[3] #This input should be the chromosome name
+start= sys.argv[4] #This input should be the position at which to start
+end= sys.argv[5]#This should be the position at which to end
+#opening file
 f = gzip.open(infile, 'r')
 #opening outfile searching for
 outfi = open(outfile, 'w')
@@ -15,10 +15,10 @@ outfi = open(outfile, 'w')
 alwrite = False
 #reading file
 fr = f.readlines()
-#Getting header & data  
+#Getting header & data
 for line in fr:
 	if line.startswith('##'):
-		continue 
+		continue
 	if line.startswith('#'):
 		outfi.write(line)
 	if look in line:
@@ -26,7 +26,7 @@ for line in fr:
 			outfi.write(line)
 			alwrite = True
 		if end in line:
-			break 
+			break
 #opening and writing outfile
 outfi.write(store)
 outfi.close()
