fba3a27582479332cc1ac8c390947e4ad24f939c
Joseline <joselinev534@gmail.com>
2019-11-14 11:38:30 -0800

    last updates *fingers crossed8

--- vcf_chunks.py
+++ vcf_chunks.py
@@ -2,11 +2,11 @@ import sys
 import gzip
 #when inputing into terminal put python2 p.py vcf.gz gene.vcf.gz chromesome.name start# end#
 #input files & outfile
-infile= sys.argv[1]#name of vcf
-outfile= sys.argv[2]#outfilename
+infile= sys.argv[1] #name of vcf
+outfile= sys.argv[2] #outfilename
 look= sys.argv[3] #This input should be the chromosome name
 start= sys.argv[4] #This input should be the position at which to start
-end= sys.argv[5]#This should be the position at which to end
+end= sys.argv[5] #This should be the position at which to end
 #opening file
 f = gzip.open(infile, 'r')
 #opening outfile
