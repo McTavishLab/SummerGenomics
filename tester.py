302aa0ff93c9d2b5c07f4556719c8fafea3be235
Joseline Velasquez <joselinev534@gmail.com>
2020-03-31 22:02:19 -0700

    updated, tells you amount of variant calls

--- tester.py
+++ tester.py
@@ -15,13 +15,14 @@ def main():
     window = int(args.window_size)
     mito_tag = "NC_006853.1"
     f = gzip.open(args.vcf, 'r')
-    filename = args.outfile+'_{}.vcf'.format(start_pos)
-    ofi = open(filename,'w+')
+    filename = args.outfile +'_{}.vcf'.format(start_pos)
+    ofi = open(filename,'w')
     end_pos= start_pos + window
+    line_count = 0
     for line in f:
         if line.startswith("##"):
                 continue
-        elif line.startswith("##"):
+        if line.startswith("#CHROM"):
                 header = line
                 ofi.write(header)
         else:
@@ -31,17 +32,22 @@ def main():
                 continue
             spline = line.split()
             basecall = int(spline[1])
-            # if start_pos < basecall < end_pos:
-            #     ofi.write(str(line)+"\n")
-            # elif basecall >= end_pos:
-            #     start_pos = math.floor(basecall / window ) * window
-            #     end_pos = start_pos + window
-            #     filename = args.outfile+"_{}.vcf".format(start_pos)
-            #     ofi.close()
-            #     ofi = open(filename, 'w+')
-            #     ofi.write(header)
-            #     end_pos = start_pos + window
-            #     ofi.write(str(basecall)+"\n")
+            if start_pos < basecall < end_pos:
+                ofi.write(str(line))
+                line_count += 1
+            elif basecall >= end_pos:
+                line_count += 1
+                start_pos = math.floor(basecall / window ) * window
+                end_pos = start_pos + window
+                filename = args.outfile+"_{}.vcf".format(int(start_pos))
+                ofi.close()
+                ofi = open(filename, 'w')
+                ofi.write(header)
+                end_pos = start_pos + window
+                ofi.write(str(line))
+    print("The total number of variant site in the vcf file is "+line_count+".")
+    f.close()
+
 
 if __name__ == '__main__':
     main()
