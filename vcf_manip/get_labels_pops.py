3dcfe33de8ca624ac4a2540c8e73f050cbfa3383
Emily Jane McTavish <ejmctavish@gmail.com>
2020-08-25 13:31:41 -0700

    Scripts for ananlyzing vcfs

---
+++ vcf_manip/get_labels_pops.py
@@ -0,0 +1,36 @@
+import os
+
+import argparse
+parser = argparse.ArgumentParser()                            
+
+parser.add_argument("--map", "-m", type=str, required=True)
+parser.add_argument("--input", "-i", type=str, required=True)
+parser.add_argument("--output", "-o", type=str, required=True)
+
+args = parser.parse_args()
+
+assert(os.path.exists(args.input))
+assert(os.path.exists(args.map))
+
+info = {}
+
+mappings = open(args.map)
+header = mappings.readline().strip().split(',')
+assert header == ["filename","idnum","individualID","pop"], header
+
+
+for lin in mappings:
+    lii = lin.strip().split(',')
+    if len(lii)==4:
+        info[lii[0]]={"label":lii[2], 'pop':lii[3]}
+
+
+fi= open(args.output,'w')
+fi.write('filename, label, pop\n')
+for samp in open(args.input):
+    samp=samp.strip()
+    assert(samp in info), samp
+    fi.write("{},{},{}\n".format(samp, info[samp]['label'],info[samp]['pop']))
+
+
+fi.close()
