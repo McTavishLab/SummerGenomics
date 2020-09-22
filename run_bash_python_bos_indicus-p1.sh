98ec165018b4b27cdd4f7a18fc4f2787d6dc6592
Joseline M. Velasquez-Reyes <51838748+jvelasquez24@users.noreply.github.com>
2020-09-22 09:14:19 -0700

    Update run_bash_python_bos_indicus-p1.sh
    updated

--- run_bash_python_bos_indicus-p1.sh
+++ run_bash_python_bos_indicus-p1.sh
@@ -3,11 +3,11 @@
 #hi
 
 start=1
-stop=100
+stop=98
 step=1
 
 for ((i=start;i<=stop;i+=step));do
-  python2 calculate_d_genomewide_cows.py -infile ../Data/summer.vcf.gz -outfile ../Results/D_Dplus_genomewide_p1bt_p2_sanga_indicus_p3_bi_p4_bb.csv -p1 bos_taurus -p2 sanga_indicus -p3 bos_indicus -p4 bison_bison -meta ../Data/meta_cow_summer.csv;
+  python2 calculate_d_dplus_genomewide_cows.py -infile vcf/ChrALL.vcf.gz -outfile d -p1 reference -p2 ndama -p3 indicine -p4 water_buffalo -meta pop_def_fi_breed.txt;
 done
 
 
